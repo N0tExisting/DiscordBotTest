795e3916d24f90ed0c3f6ec40b4d6417b2d0fa8f
NotExisting <61290107+N0tExisting@users.noreply.github.com>
2020-11-04 15:35:05 +0100

    Added New Property
    Use This to Check for mutiple owners

--- DiscordBotTest/ConfigJson.cs
+++ DiscordBotTest/ConfigJson.cs
@@ -4,27 +4,19 @@ namespace DiscordBotTest
 {
 	public struct ConfigJson
 	{
+#region Properties
 		[JsonProperty("token")]
 		public string Token { get; private set; }
 		[JsonProperty("prefix")]
 		public string Prefix { get; private set; }
-		public override bool Equals(object obj)
-		{
-			return obj is ConfigJson json &&
-				   Token == json.Token &&
-				   Prefix == json.Prefix;
-		}
-		public override int GetHashCode()
-		{
-			return HashCode.Combine(Token, Prefix);
-		}
-		public static bool operator ==(ConfigJson left, ConfigJson right)
-		{
-			return left.Equals(right);
-		}
-		public static bool operator !=(ConfigJson left, ConfigJson right)
-		{
-			return !(left == right);
-		}
+		[JsonProperty("OwnerIDs")]
+		public ulong[] OwnerIDs { get; private set; }
+#endregion
+#region Overrides
+		public override bool Equals(object obj) => obj is ConfigJson json && Token == json.Token && Prefix == json.Prefix && OwnerIDs == json.OwnerIDs;
+		public static bool operator ==(ConfigJson left, ConfigJson right) => left.Equals(right);
+		public static bool operator !=(ConfigJson left, ConfigJson right) => !(left == right);
+		public override int GetHashCode() => HashCode.Combine(Token, Prefix, OwnerIDs);
+#endregion
 	}
 }
